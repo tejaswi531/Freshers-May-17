9ed0eda61320e801ab35b9c36cde02efae1c9294
tejaswi531 <54668200+tejaswi531@users.noreply.github.com>
2022-05-17 15:07:30 +0530

    Add files via upload

---
+++ HelloWorld/src/hello/Helloworld.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class Helloworld {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 System.out.println("hello world");
+
+	}
+
+
+	
+	
+
+}

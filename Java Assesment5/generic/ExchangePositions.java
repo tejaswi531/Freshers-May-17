78a2e3ca007b1349942f359a2361372eeb9f5e55
tejaswi531 <54668200+tejaswi531@users.noreply.github.com>
2022-05-30 16:53:08 +0530

    Add files via upload

---
+++ Java Assesment5/generic/ExchangePositions.java
@@ -0,0 +1,20 @@
+package generic;
+
+public class ExchangePositions {
+	public static final<T>void swap(T[]a,int i,int j){
+		T t =a[i];
+		a[i]=a[j];
+		a[j]=t;
+
+	}	
+
+	public static void main(String[] args) {
+		Integer [] a = {1,5,3,7};
+		swap(a,1,2);
+		for(Integer i:a)
+			System.out.println(i+" ");
+		
+
+	}
+
+}

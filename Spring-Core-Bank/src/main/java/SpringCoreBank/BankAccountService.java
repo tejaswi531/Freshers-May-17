5d15f864443be0865840a5f77cad39405976143d
tejaswi531 <54668200+tejaswi531@users.noreply.github.com>
2022-06-14 17:38:46 +0530

    Add files via upload

---
+++ Spring-Core-Bank/src/main/java/SpringCoreBank/BankAccountService.java
@@ -0,0 +1,7 @@
+package SpringCoreBank;
+public interface BankAccountService {
+    public double withdraw(long accountId, double balance);
+    public double deposit(long accountId, double balance);
+    public double getBalance(long accountId);
+    public boolean fundTransfer(long fromAccount, long toAccount, double amont);
+}
